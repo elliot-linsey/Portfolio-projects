e977e2463044bd82018f7f100f45ac033efd0d92
elliot-linsey <78038483+elliot-linsey@users.noreply.github.com>
2021-08-29 15:05:32 +0100

    Add files via upload

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -8,18 +8,24 @@ def word_choice():
     choice_list.append(x)
   return choice_list
 
+prev_ch = []
 choice = word_choice()
 empty_lst = ['_']*len(choice)
 
 def word_input():
   ch = input('What is your choice? ')
-  word_input.count -= 1
+  if ch not in choice:
+    prev_ch.append(ch)
+    word_input.count -= 1
+  elif ch in choice:
+      word_input.count += 0
   return ch
 
 word_input.count = 10
 
 def hangman():
   #print(choice)
+  print(f'Previous choices: {prev_ch}')
   print(empty_lst)
   if empty_lst == choice:
     return 'You win!'
